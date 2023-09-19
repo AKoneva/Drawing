c1ed3447a416785288d0eff0ac3b86811851cbe1
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-19 12:47:16 +0900

    clean code

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,44 +7,6 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    var insetAmount = 0.0
-    
-    // More natural way to work with degrees
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-}
-
 struct Flower: Shape {
     // How much to move this petal away from the center
     var petalOffset: Double = -20
@@ -85,42 +47,30 @@ struct ContentView: View {
     @State private var isChecked = false
     
     var body: some View {
-        ZStack {
-            //            Circle()
-            //                .stroke(.gray, lineWidth: 5)
-            //
-            //            Triangle()
-            //                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-            //                    .frame(width: 300, height: 300)
-            //
-            //            Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            //                .strokeBorder(.blue, lineWidth: 40)
-            
-            VStack {
-                if isChecked {
-                    Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                        .fill(.red, style: FillStyle(eoFill: true))
-                } else {
-                    Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                        .stroke(.red, lineWidth: 1)
-                }
-               
-                Text("Offset")
-                Slider(value: $petalOffset, in: -40...40)
-                    .padding([.horizontal, .bottom])
-                
-                Text("Width")
-                Slider(value: $petalWidth, in: 0...100)
-                    .padding(.horizontal)
-                Toggle(isOn: $isChecked) {
-                    Text("Fill with color")
-                }
-                .padding()
+        VStack {
+            if isChecked {
+                Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                    .fill(.red, style: FillStyle(eoFill: true))
+            } else {
+                Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                    .stroke(.red, lineWidth: 1)
             }
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+            Toggle(isOn: $isChecked) {
+                Text("Fill with color")
+            }
+            .padding()
         }
-        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
