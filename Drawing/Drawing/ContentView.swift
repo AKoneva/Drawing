76a8b32ced488a444f9923a056462a05b4d90620
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-19 12:46:35 +0900

    Learn drawing shapes, how path works

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,15 +7,118 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount = 0.0
+    
+    // More natural way to work with degrees
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
         }
-        .padding()
+        
+        // now send the main path back
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var isChecked = false
+    
+    var body: some View {
+        ZStack {
+            //            Circle()
+            //                .stroke(.gray, lineWidth: 5)
+            //
+            //            Triangle()
+            //                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            //                    .frame(width: 300, height: 300)
+            //
+            //            Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            //                .strokeBorder(.blue, lineWidth: 40)
+            
+            VStack {
+                if isChecked {
+                    Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                        .fill(.red, style: FillStyle(eoFill: true))
+                } else {
+                    Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                        .stroke(.red, lineWidth: 1)
+                }
+               
+                Text("Offset")
+                Slider(value: $petalOffset, in: -40...40)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Width")
+                Slider(value: $petalWidth, in: 0...100)
+                    .padding(.horizontal)
+                Toggle(isOn: $isChecked) {
+                    Text("Fill with color")
+                }
+                .padding()
+            }
+        }
+        
     }
 }
 
